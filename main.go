91602401a08c6494d9c5d4b1680451cf9e62f872
Andresito126 <creed126ax@gmail.com>
2025-02-04 16:01:59 -0600

    Finished

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"log"
 
+	"time"
+
+	"github.com/Andresito126/go-servidor-replicador/principal"
+	replicacion "github.com/Andresito126/go-servidor-replicador/server2"
 )
 
 func main() {
+	// server main
+	go func() {
+		log.Println("Servidor principal corriendo en :8080")
+		if err := principal.SetupRouter().Run(":8080"); err != nil {
+			log.Fatal("Error en servidor principal:", err)
+		}
+	}()
 
+	time.Sleep(2 * time.Second)
+
+	
+	//server de replica
+	go func() {
+		log.Println("🔄 Servidor de replica corriendo en :8081")
+		if err := replicacion.SetupRouter().Run(":8081"); err != nil {
+			log.Fatal("Error en servidor de replica:", err)
+		}
+	}()
+
+	
+	go replicacion.InicioReplica()
+
+	
+	select {}
 }
